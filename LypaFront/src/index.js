b077e557b69d7168d7ee0db0b24aebf21f7ed252
Oleksii Palamarchuk <cemper300@gmail.com>
2023-06-26 21:41:16 +0300

    Add some css 🤢

--- LypaFront/src/index.js
+++ LypaFront/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 
