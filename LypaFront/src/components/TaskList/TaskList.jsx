77a03f9bd0315ceac9b539441cffff39be83b9f9
Oleksii Palamarchuk <cemper300@gmail.com>
2023-07-30 00:21:45 +0300

    Add table with sortable headers for task list page

--- LypaFront/src/components/TaskList/TaskList.jsx
+++ LypaFront/src/components/TaskList/TaskList.jsx
@@ -1,52 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getTaskList } from "../../rest/tasks.rest";
+import styles from "./TaskList.module.css";
 
 function TaskList() {
-    // Assuming you receive the task list as a prop
-    const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState([]);
+  const [sortBy, setSortBy] = useState(""); // State to keep track of the current sorting option
 
-    useEffect(() => {
-        async function prepareTasks() {
-            try {
-                const data = {
-                    chatId: 12314213
-                };
-                const result = await getTaskList(data);
-                setTaskList(result);
-            } catch (error) {
-                console.error("Error fetching tasks:", error);
-            }
-        }
-  
-        prepareTasks();
-    }, []);
-    console.log(taskList);
-    return (
-        <div>
-            <h2>Task List</h2>
-            <ul>
-                {taskList.map((taskObj, index) => {
-                    const taskName = Object.keys(taskObj)[0];
-                    const userScores = taskObj[taskName];
+  useEffect(() => {
+    async function prepareTasks() {
+      try {
+        const result = await getTaskList();
 
-                    return (
-                        <li key={index}>
-                            <strong>{taskName}:</strong>
-                            <ul>
-                                {Object.entries(userScores).map(([userName, score]) => (
-                                    <li key={userName}>
-                                        {userName}: {score}
-                                    </li>
-                                ))}
-                            </ul>
-                        </li>
-                    );
-                })}
-            </ul>
-            <button><Link to={"/"} >Cancel</Link></button>
-        </div>
-    );
+        // Convert the taskList object to an array of objects with properties for task, user, and result
+        const tasksArray = [];
+        result.forEach((taskObj) => {
+          const taskName = Object.keys(taskObj)[0];
+          const userAndScores = taskObj[taskName];
+
+          Object.entries(userAndScores).forEach(([user, result]) => {
+            tasksArray.push({ task: taskName, user, result });
+          });
+        });
+
+        setTaskList(tasksArray);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    }
+
+    prepareTasks();
+  }, []);
+
+  const handleSort = (column) => {
+    if (column === sortBy) {
+      setTaskList([...taskList].reverse());
+    } else {
+      setTaskList([...taskList].sort((a, b) => (a[column] > b[column] ? 1 : -1)));
+    }
+    setSortBy(column);
+  };
+
+  return (
+    <div>
+    <h2>Task List</h2>
+    <div className={styles.tableContainer}>
+      <table className={styles.taskTable}>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("task")}>Task</th>
+            <th onClick={() => handleSort("user")}>User</th>
+            <th onClick={() => handleSort("result")}>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskList.map((taskObj, index) => (
+            <tr key={index}>
+              <td>{taskObj.task}</td>
+              <td>{taskObj.user}</td>
+              <td>{taskObj.result}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <button>
+      <Link to={"/"}>Cancel</Link>
+    </button>
+  </div>
+);
 }
 
 export default TaskList;
