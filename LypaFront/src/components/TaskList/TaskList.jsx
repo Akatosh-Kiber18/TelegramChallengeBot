bbdf55798eb9f0a012f45e8cb9dec812f2bce42a
Oleksii Palamarchuk <cemper300@gmail.com>
2023-08-21 22:22:27 +0300

    Add get tasks

--- LypaFront/src/components/TaskList/TaskList.jsx
+++ LypaFront/src/components/TaskList/TaskList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { getTaskList, getTask } from "../../rest/tasks.rest";
+import { getTaskList, getTasks } from "../../rest/tasks.rest";
 import styles from "./TaskList.module.css";
 
 function TaskList({tgApp}) {
